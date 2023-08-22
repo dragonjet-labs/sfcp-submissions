4d9179699d4d3ad88eb1ffe2b5ff2ccedd5cf886
dragonjet <jetriconew@gmail.com>
2023-08-21 18:47:23 -0700

    Trigger GitHub Action

--- web/index.js
+++ web/index.js
@@ -3,25 +3,31 @@ const express = require('express')
 const cors = require('cors')
 const multer  = require('multer')
 const AWS = require('aws-sdk')
+const { Octokit } = require("octokit")
 
 const app = express()
 app.use(cors())
 const port = 3000
 const upload = multer({ dest: 'uploads/' })
 
+// DigitalOcean
 const { DO_SPACES_KEY, DO_SPACES_SECRET } = process.env;
 const spacesEndpoint = new AWS.Endpoint('sfo3.digitaloceanspaces.com');
 const s3 = new AWS.S3({
-  region: 'sfo3',
   endpoint: spacesEndpoint,
   accessKeyId: DO_SPACES_KEY,
   secretAccessKey: DO_SPACES_SECRET,
 });
 
+// GitHub
+const octokit = new Octokit({ auth: GITHUB_TOKEN })
+
+// Submit Endpoint
 app.post('/submit', upload.single('espFile'), async (req, res) => {
   const { originalname, path: filePath } = req.file;
 
-  const upload = await new Promise((resolve, reject) => {
+  // Upload ESP File to DigitalOcean Spaces
+  await new Promise((resolve, reject) => {
     let fileStream = fs.createReadStream(filePath)
     s3.putObject({
       Bucket: 'sfcp-submissions',
@@ -35,14 +41,24 @@ app.post('/submit', upload.single('espFile'), async (req, res) => {
     });
   });
 
-  res.json({
-    branchname: req.body.branchname,
-    espFile: req.file,
-    message: req.body.message,
-    upload,
+  // Trigger GitHub Actions workflow
+  await octokit.request('POST /repos/dragonjet-labs/sfcp-flows/actions/workflows/spriggit-win/dispatches', {
+    owner: 'dragonjet-labs',
+    repo: 'sfcp-flows',
+    workflow_id: 'spriggit-win',
+    ref: 'main',
+    inputs: {
+      branchname: req.body.branchname,
+      message: req.body.message,
+      espfile: `https://sfcp-submissions.sfo3.digitaloceanspaces.com/${originalname}`,
+    },
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
   })
 })
 
+// Web app listener
 app.listen(port, () => {
   console.log(`SFCP Submissions Web on Port ${port}`)
 })
