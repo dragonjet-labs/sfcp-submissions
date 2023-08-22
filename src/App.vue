a4959023f6a6f437b960a143fa0e924d47c18c9d
dragonjet <jetriconew@gmail.com>
2023-08-21 17:37:11 -0700

    Home page: file uploader

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, watch } from 'vue'
+
+const branchname = ref(0);
+watch(branchname, (newX) => {
+  const stripped = newX.replace(/[^a-z0-9]/gi, '');
+  branchname.value = stripped;
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-sheet width="600" class="mx-auto">
+    <v-form fast-fail @submit.prevent="submit" v-if="!submitting">
+      <v-text-field
+        :rules="[rules.required]"
+        pattern="[a-zA-Z0-9]+"
+        v-model="branchname"
+        label="Branch Name">
+      </v-text-field>
+
+      <v-file-input
+        id="espFile"
+        :rules="[rules.required]"
+        accept=".esm,.esp,.esl,.png"
+        label="ESM/ESP/ESL File">
+      </v-file-input>
+
+      <v-text-field
+        :rules="[rules.required]"
+        v-model="message"
+        label="Short Message">
+      </v-text-field>
+
+      <v-btn type="submit" block class="mt-2">Submit</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
 
+<script>
+import axios from 'axios';
+
+export default {
+  data: () => ({
+    branchname: '',
+    message: '',
+    espFile: null,
+    submitting: false,
+    rules: {
+      required: value => !!value || 'Field is required',
+    },
+  }),
+  methods: {
+    async submit () {
+      this.submitting = true;
+      let formData = new FormData();
+      const fileInput = document.querySelector('#espFile');
+      formData.append('pic', fileInput.files[0]);
+      const res = axios.post('https://jetri.co', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log('res', res);
+    },
+  },
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
